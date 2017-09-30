d9fc7886f083268fd53da875a07aaf15c986bd7a
Andrei <andreicrimson@gmail.com>
2017-09-30 03:23:42 -0700

    configuration file for pm2 to run automatically and SSH the repo

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'Log-server',
+      script: './index.js'
+    }],
+    deploy: {
+      production: {
+        user: 'colourhood',
+        host: 'ec2-52-14-241-17.us-east-2.compute.amazonaws.com',
+        key: '~/.ssh/aws-colourhood-key-pair.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:Colourhood/Log-mysql-nodejs-database.git',
+        path: '/home/colourhood/COLOURHOOD/Log-mysql-nodejs-database/',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
