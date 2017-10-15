8a0f43d1df911cce035b2c647d37d02ad5b7d16c
Andrei <andreicrimson@gmail.com>
2017-10-15 00:48:54 -0700

    slight refactor to server creation, and socket is available through server routes 'app.io'

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 require('app-module-path').addPath(__dirname);
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
 
-const routes = require('routes');
-const app = express();
+const bodyParser = require('body-parser'),
+      routes = require('routes'),
+      app = require('express')()
+      server = require('http').Server(app),
+      socketio = require('socket.io')(server);
 
 app.use(bodyParser.json({ limit: '2mb' }));
 app.use('/api', routes);
+app.io = socketio;
 
 app.get('/', (request, response) => {
     response.status(200).json({ Home: 'Colourhood presents the log database project' });
 });
 
-http.createServer(app).listen(7555, () => {
+server.listen(7555, () => {
     console.log('Server running on port 7555');
 });
