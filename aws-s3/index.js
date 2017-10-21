54cfd6497c64af8d3c4b0734dd6c11fb0d61d98b
Andrei <andreicrimson@gmail.com>
2017-10-21 04:18:11 -0700

    remove objects from scope of access.

--- aws-s3/index.js
+++ aws-s3/index.js
@@ -1,6 +1,4 @@
 const config = require('aws-s3/config');
 const actions = require('aws-s3/actions');
-const keys = require('aws-s3/keys');
-const ext = require('aws-s3/extensions')
 
 module.exports = { config, actions };
