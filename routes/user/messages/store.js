20fc4d3f1aef931ae4e4f2a39d2d003becb6a4fb
Andrei <andreicrimson@gmail.com>
2017-10-21 04:17:49 -0700

    successfully send image profile picture for conversation with friend. - Might have to refactor client as well 'get rid of the array'.

--- routes/user/messages/store.js
+++ routes/user/messages/store.js
@@ -1,4 +1,7 @@
 const knex = require('knex')(require('knexfile'));
+const aws = require('aws-s3');
+
+const { actions } = aws;
 
 function getHomeMessages({ username }) {
     console.log('Trying to get the messages from SQL store');
@@ -14,12 +17,26 @@ function getHomeMessages({ username }) {
             }).map((values) => {
                 const friend = values.sentTo;
                 // Processing of fetching most recent message conversation between friend and user
-                return knex('messages')
-                      .where({ 'sentBy': username, 'sentTo': friend })
-                      .orWhere({ 'sentBy': friend, 'sentTo': username })
-                      .select('sentTo', 'sentBy', 'message')
-                      .orderBy('created_at', 'desc')
-                      .limit(1);
+                const awsPromise = actions.getProfileImage(friend);
+                const knexPromise = knex('messages')
+                                    .where({ 'sentBy': username, 'sentTo': friend })
+                                    .orWhere({ 'sentBy': friend, 'sentTo': username })
+                                    .select('sentTo', 'sentBy', 'message')
+                                    .orderBy('created_at', 'desc')
+                                    .limit(1);
+
+                return Promise.all([knexPromise, awsPromise]).then((data) => {
+                    const { sentTo, sentBy, message } = data[0][0]; //Database data
+                    const { success, image, errorMessage } = data[1]; //Aws Image Object
+
+                    if (success) { //Image exists for user!
+                        return [{ sentTo, sentBy, message, image }]; 
+                    } else { //Image doesn't exist for user :(
+                        return [{ sentTo, sentBy, message, errorMessage }];
+                    }
+                }).catch((error) => {
+                    console.log(`An error occurred - fetching friend profile pic: ${error}`);
+                });
 
             });
 }
