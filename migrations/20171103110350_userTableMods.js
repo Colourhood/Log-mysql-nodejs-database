17220a89bb044c46111b513f3a4422aac9900861
Andrei <andreicrimson@gmail.com>
2017-11-03 13:37:07 -0700

    made some modifications to user properties in the schema

---
+++ migrations/20171103110350_userTableMods.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('user', (t) => {
+      t.string('email_address').notNullable();
+      t.dropColumn('username');
+
+      t.string('first_name').notNullable();
+      t.string('last_name').notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.table('user', (t) => {
+        t.dropColumn('email_address');
+        t.dropColumn('first_name');
+        t.dropColumn('last_name');
+        t.string('username').notNullable();
+    });
+};
