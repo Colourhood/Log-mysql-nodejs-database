0446c2acf0477a7b93c20b5eb05dbec634a69171
Andrei <andreicrimson@gmail.com>
2017-11-05 23:43:39 -0800

    remove debugging

--- aws-s3/actions.js
+++ aws-s3/actions.js
@@ -4,9 +4,9 @@ const keys = require('aws-s3/keys');
 function listAllObjects() {
 	S3.listObjects((error, data) => {
 		if (error) {
-			console.log(`There was an error: ${error}`);
+			//console.log(`There was an error: ${error}`);
 		} else {
-			console.log(`Succesful data fetch: ${JSON.stringify(data)}`);
+			//console.log(`Succesful data fetch: ${JSON.stringify(data)}`);
 		}
 	});
 }
@@ -24,7 +24,7 @@ function getProfileImage(user_email) {
 		//console.log(object);
 		return Promise.resolve({ image: object.Body.toString('base64') });
 	}).catch((error) => {
-		console.log('Error fetching object from bucket: '+error);
+		//console.log('Error fetching object from bucket: '+error);
 		return Promise.resolve({ error: error.message });
 	});
 }
